7ab89223766c503ec7e48d5ca31b9ca593e3771f
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-05-30 08:53:59 -0300

    welckson valentim - feat:App.js. estrutura componente App, inserindo componente Login

--- list-of-contacts/src/App.js
+++ list-of-contacts/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import Context from './Context/Context';
+import Login from './Pages/Login';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context>
+        <Login />
+    </Context>
   );
 }
 
